844209f22062247af2c7a420f2f2ec11cdbd3196
Miguel Rios <miguelriosoliveira@gmail.com>
2021-06-22 23:06:50 -0300

    Setting up database connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import express from 'express';
 import 'dotenv/config';
+import 'reflect-metadata';
+
+import express from 'express';
+
+import './database';
 
 const { PORT } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => console.log('🚀 Listening on port' + PORT));
+app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}`));
